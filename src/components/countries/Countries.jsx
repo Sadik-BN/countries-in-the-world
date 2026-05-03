33e00fd2773cb0cf9b992d90e5ccd95d843be167
Sadik-BN <sadikonline4@gmail.com>
2026-05-03 07:20:54 +0600

    Key prop error removed

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -6,25 +6,25 @@ function Countries({ countriesPromise }) {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
 
-    const[visitCount,setVisitCount] = useState(0);
+    const [visitCount, setVisitCount] = useState(0);
 
-    const[visitFlagList,setVisitFlagList] = useState([]);
+    const [visitFlagList, setVisitFlagList] = useState([]);
 
-    const handleVisitFlag = (flag)=>{
-        let newList = [...visitFlagList,flag];
+    const handleVisitFlag = (flag) => {
+        let newList = [...visitFlagList, flag];
         setVisitFlagList(newList);
     }
 
     return (
         <>
 
-            <h1 style={{textAlign:'center'}}>Countries In The World</h1>
-            <h2 style={{textAlign:'center'}}>Total: {countries.length}</h2>
-            <h2 style={{textAlign:'center'}}>Visited: {visitCount}</h2>
-            <h2 style={{textAlign:'center'}}>Flags Visited: {visitFlagList.length}</h2>
+            <h1 style={{ textAlign: 'center' }}>Countries In The World</h1>
+            <h2 style={{ textAlign: 'center' }}>Total: {countries.length}</h2>
+            <h2 style={{ textAlign: 'center' }}>Visited: {visitCount}</h2>
+            <h2 style={{ textAlign: 'center' }}>Flags Visited: {visitFlagList.length}</h2>
 
             {
-                visitFlagList.map(flag=> <img className="visited-flag" src={flag}></img>)
+                visitFlagList.map((flag, index) => <img key={index} className="visited-flag" src={flag}></img>)
             }
 
             <div className="card-container">
@@ -43,7 +43,7 @@ function Countries({ countriesPromise }) {
                 {/* This is for everything in one component */}
 
                 {
-                    countries.map(country => <Country key={country.ccn3.ccn3} country={country} visitCount = {visitCount} setVisitCount={setVisitCount} handleVisitFlag={handleVisitFlag}></Country>)
+                    countries.map(country => <Country key={country.ccn3.ccn3} country={country} visitCount={visitCount} setVisitCount={setVisitCount} handleVisitFlag={handleVisitFlag}></Country>)
                 }
 
             </div>
